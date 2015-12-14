ab91ee95381b57103b91b601b73bc139ff0e50be
Lilly Ghassemieh <lillygh@gmail.com>
2015-12-14 14:37:23 -0800

    added user model

---
+++ app/models/user.js
@@ -0,0 +1,51 @@
+//packages for user model
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema,
+	bcrypt = require('bcrypt-node.js');
+
+//user schema 
+var UserSchema = new Schema({
+	name: String,
+	email: String,
+	username: {type: String, required: true, index: {unique: true}},
+	password: {type: String, required: true, select: false}
+})
+
+//hash password before user is saved
+UserSchema.pre('save', function(next){
+	var user = this;
+	//hash pw only if pw has been changed or user is new
+	if (!user.isModified('password'))
+		return next()
+
+	//generate the salt
+	bcrypt.hash(user.password, null, null, function(err, hash){
+		if (err) 
+			return next(err)
+
+		//change the pw to the hash version
+		user.password = hash;
+		next()
+	})
+})
+
+//method to compare given pw with database hash
+UserSchema.methods.comparePassword = function(password){
+	var user = this;
+	return bcrypt.compareSync(password, user.password)
+}
+
+//return the model
+module.exports = mongoose.model('User', UserSchema)
+
+
+
+
+
+
+
+
+
+
+
+
