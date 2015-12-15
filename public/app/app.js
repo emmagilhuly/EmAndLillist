681e41a0c10bd5193dc89df7d8b77cd97c496b90
Emma Gilhuly <emmagilhuly@me.com>
2015-12-15 12:34:00 -0800

    item controller user controller

--- public/app/app.js
+++ public/app/app.js
@@ -1,2 +1,8 @@
 angular.module('emAndLilApp', ['ngAnimate', 'appRoutes', 'authService', 'mainCtrl', 'userCtrl', 'itemCtrl', 'userService', 'itemService'])
 
+//application config to integrate token into request
+.config(function($httpProvider){
+
+  //attach our auth interceptor to the http requests
+  $httpProvider.interceptors.push('AuthInterceptor');
+})
--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -4,10 +4,16 @@ angular.module('userCtrl', ['userService'])
 //create a controller and inject the User factory
 .controller('userController', function(User){
 	var self = this
+
+	//set a processing variable to show loading things
+	self.processing = true;
+
 	//get all the users
 	User.all()
 		//promise object
 		.success(function(data){
+			//when all the users come back, remove processing variable
+			self.processing = false
 			//bind the data to a controller variable, this is from userService
 			self.user = data;
 		})
