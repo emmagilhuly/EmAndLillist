a521df626922b14a18be7d09f4d6c5d4318edae6
Lilly Ghassemieh <lillygh@gmail.com>
2015-12-14 16:22:51 -0800

    added app.js and index.html files

---
+++ js/app.js
@@ -0,0 +1,17 @@
+angular.module('emAndLilApp', [])
+
+.controller('mainController', function(){
+	//bind this to self
+	var self = this
+
+	//define variables and objects on this so it's available to views
+	//define a basic variable
+	self.message = 'hey come and see how good i look'
+	//define a list of items
+	self.items = {
+		{name: 'macbook pro', color: 'silver', price: 1000},
+		{name: 'imac', color: 'silver', price: 1500},
+		{name: 'macbook air', color: 'black', price: 500}
+	}
+})
+
