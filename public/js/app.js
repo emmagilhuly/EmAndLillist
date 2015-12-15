e4507bc59f02aa11819edb6f3a304702a823d63e
Emma Gilhuly <emmagilhuly@me.com>
2015-12-14 17:11:44 -0800

    added module and controller and big message to home page

--- public/js/app.js
+++ public/js/app.js
@@ -6,27 +6,49 @@ angular.module('emAndLilApp', [])
 
 	//define variables and objects on this so it's available to views
 	//define a basic variable
-	self.message = 'hey come and see how good i look'
-	//define a list of items
-	self.items = [
-		{name: 'macbook pro', color: 'silver', price: 1000},
-		{name: 'imac', color: 'silver', price: 1500},
-		{name: 'macbook air', color: 'black', price: 500}
-	]
+	self.bigMessage = 'whassup whassup'
+})
 
-	//info that comes from our form
-	self.itemData = {}
-	self.addItem = function(){
-		//add a item to list
-		self.items.push({
-			name: self.itemData.name,
-			color: self.itemData.color,
-			price: self.itemData.price,
-		})
-
-		//after our item has been added, clear the form
-		self.itemData = {}
-	}
+//home page specific controller
+.controller('homeController', function(){
+	var self = this;
+	self.message = 'This is the home page!'
 
 })
 
+.controller('aboutController', function(){
+	var self = this;
+	self.message = 'About Page!'
+
+})
+
+.controller('homeController', function(){
+	var self = this;
+	self.message = 'This is the home page!'
+
+})
+
+
+
+//define a list of items
+// 	self.items = [
+// 		{name: 'macbook pro', color: 'silver', price: 1000},
+// 		{name: 'imac', color: 'silver', price: 1500},
+// 		{name: 'macbook air', color: 'black', price: 500}
+// 	]
+//
+// 	//info that comes from our form
+// 	self.itemData = {}
+// 	self.addItem = function(){
+// 		//add a item to list
+// 		self.items.push({
+// 			name: self.itemData.name,
+// 			color: self.itemData.color,
+// 			price: self.itemData.price,
+// 		})
+//
+// 		//after our item has been added, clear the form
+// 		self.itemData = {}
+// 	}
+//
+// })
