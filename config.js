f5ee8e7b43900afb355321d18ae3acdea348aacf
Emma Gilhuly <emmagilhuly@me.com>
2015-12-15 15:39:51 -0800

    everything is working i love my life

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
 	'port': process.env.PORT || 5000,
-	'database': 'mongodb://admin:admin@ds053954.mongolab.com:53954/lillysdb',
+	// 'database': 'mongodb://admin:admin@ds053954.mongolab.com:53954/lillysdb',
+	'database': 'mongodb://localhost:27017/ellist',
 	'secret': 'lilyandemma'
 }
