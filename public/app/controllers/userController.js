19edb905c48b1a63170433add8e5d70d086df311
Lilly Ghassemieh <lillygh@gmail.com>
2015-12-15 12:52:12 -0800

    add delete functionality to all users

--- public/app/controllers/userController.js
+++ public/app/controllers/userController.js
@@ -4,8 +4,6 @@ angular.module('userCtrl', ['userService'])
 //create a controller and inject the User factory
 .controller('userController', function(User){
 	var self = this
-
-	//set a processing variable to show loading things
 	self.processing = true;
 
 	//get all the users
@@ -17,4 +15,26 @@ angular.module('userCtrl', ['userService'])
 			//bind the data to a controller variable, this is from userService
 			self.user = data;
 		})
+
+	//function to delete user
+	self.deleteUser = function(id) {
+		//set a processing variable to show loading things
+		self.processing = true;
+
+		//accepts the user id as a parameter
+		User.delete(id)
+			.success(function(data){
+			//get all the users
+			User.all()
+				//promise object
+				.success(function(data){
+					//when all the users come back, remove processing variable
+					self.processing = false
+					//bind the data to a controller variable, this is from userService
+					self.users = data;
+				})
+
+			})
+	}	
+
 })
