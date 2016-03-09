5cf22f4dd46db3e397baaf2ce401003c58e4a825
Emma Gilhuly <emmagilhuly@me.com>
2016-03-09 08:16:05 -0500

    responsive

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'port': process.env.PORT || 7000,
+	'port': process.env.PORT || 4444,
 	'database': 'mongodb://admin:admin@ds053954.mongolab.com:53954/lillysdb',
 	// 'database': 'mongodb://localhost:27017/ellist',
 	'secret': 'lilyandemma'
