8ef5bf05f29fac3b0b119a2969b65664e4102ebd
Emma Gilhuly <emmagilhuly@me.com>
2016-03-25 10:10:12 -0400

    cleaning

--- app/routes/api.js
+++ app/routes/api.js
@@ -14,14 +14,11 @@ module.exports = function(app, express) {
 
 	// route to authenticate a user (POST http://localhost:8080/api/authenticate)
 	apiRouter.post('/authenticate', function(req, res) {
-
-	  // find the user
+	  // find the user, select the username and password specifically
 	  User.findOne({
 	    username: req.body.username
 	  }).select('name username password').exec(function(err, user) {
-
 	    if (err) throw err;
-
 	    // no user with that username was found
 	    if (!user) {
 	      res.json({
@@ -29,7 +26,6 @@ module.exports = function(app, express) {
 	      	message: 'Authentication failed. User not found.'
 	    	});
 	    } else if (user) {
-
 	      // check if password matches
 	      var validPassword = user.comparePassword(req.body.password);
 	      if (!validPassword) {
@@ -38,7 +34,6 @@ module.exports = function(app, express) {
 	        	message: 'Authentication failed. Wrong password.'
 	      	});
 	      } else {
-
 	        // if user is found and password is right
 	        // create a token
 	        var token = jwt.sign({
@@ -48,7 +43,6 @@ module.exports = function(app, express) {
 	        }, superSecret, {
 	          expiresInMinutes: 1440 // expires in 24 hours
 	        });
-
 	        // return the information including token as JSON
 	        res.json({
 	          success: true,
@@ -56,9 +50,7 @@ module.exports = function(app, express) {
 	          token: token
 	        });
 	      }
-
 	    }
-
 	  });
 	});
 
@@ -108,16 +100,12 @@ module.exports = function(app, express) {
 	apiRouter.use(function(req, res, next) {
 		// do logging
 		console.log('Somebody just came to our app!');
-
 	  // check header or url parameters or post parameters for token
 	  var token = req.body.token || req.query.token || req.headers['x-access-token'];
-
 	  // decode token
 	  if (token) {
-
 	    // verifies secret and checks exp
 	    jwt.verify(token, superSecret, function(err, decoded) {
-
 	      if (err) {
 	        res.status(403).send({
 	        	success: false,
@@ -126,20 +114,16 @@ module.exports = function(app, express) {
 	      } else {
 	        // if everything is good, save to request for use in other routes
 	        req.decoded = decoded;
-
 	        next(); // make sure we go to the next routes and don't stop here
 	      }
 	    });
-
 	  } else {
-
 	    // if there is no token
 	    // return an HTTP response of 403 (access forbidden) and an error message
    	 	res.status(403).send({
    	 		success: false,
    	 		message: 'No token provided.'
    	 	});
-
 	  }
 	});
 
